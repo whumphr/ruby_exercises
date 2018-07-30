b92b9fdcd526e45aa9a283795e4c7be43f0be5e2
Will Humphrey <wahumphrey91@gmail.com>
2018-07-30 21:16:50 +0900

    Changed variable for better readability

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,22 +1,23 @@
 def bubble_sort(arr)
-    j = 1
-    while j != 0
-        changes = 0
+    changes = 1
+    while changes != 0
+        change_record = 0
         i = 0
         while i < arr.length - 1 
             if arr[i] > arr[i+1]
                 arr[i], arr[i+1] = arr[i+1], arr[i]
                 i += 1
-                changes += 1   
+                change_record += 1   
             else
                 i += 1    
             end
         end
-        j = changes
+        changes = change_record
     end
     arr
 end
 
-print bubble_sort([4,3,78,2,0,2]).to_s + "\n"
-print bubble_sort([1, 2, 3, 4, 6, 5]).to_s + "\n"
-print bubble_sort([5, 4, 3, 2, 1]).to_s + "\n"
+p bubble_sort([4,3,78,2,0,2])
+p bubble_sort([1, 2, 3, 4, 6, 5])
+p bubble_sort([5, 4, 3, 2, 1])
+
